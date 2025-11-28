cf93b7c1896a8c357d2db0b66e3e218471b76e10
ali alaei <ali.alaei.tabatabaei@gmail.com>
2025-11-28 12:33:35 +0100

    add compnent files

---
+++ components/veml6040/sensor.py
@@ -0,0 +1,121 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import i2c, sensor, sensirion_common
+from esphome.const import (
+    CONF_ID,
+    CONF_ILLUMINANCE,
+    CONF_COLOR_TEMPERATURE,
+    CONF_RED,
+    CONF_GREEN,
+    CONF_BLUE,
+    CONF_WHITE,
+    CONF_GLASS_ATTENUATION_FACTOR,
+    CONF_INTEGRATION_TIME,
+    STATE_CLASS_MEASUREMENT,
+    UNIT_LUX,
+    UNIT_KELVIN,
+    ICON_LIGHTBULB,
+    ICON_THERMOMETER,
+    DEVICE_CLASS_ILLUMINANCE,
+)
+
+CODEOWNERS = ["@airlytix"]
+DEPENDENCIES = ["i2c"]
+
+veml6040_ns = cg.esphome_ns.namespace("veml6040")
+
+VEML6040Component = veml6040_ns.class_(
+    "VEML6040Component", cg.PollingComponent, i2c.I2CDevice
+)
+
+
+VEML6040IntegrationTime = veml6040_ns.enum("VEML6040IntegrationTime")
+VEML6040_INTEGRATION_TIMES = {
+    "40ms": VEML6040IntegrationTime.VEML6040_INTEGRATION_TIME_40MS,
+    "80ms": VEML6040IntegrationTime.VEML6040_INTEGRATION_TIME_80MS,
+    "160ms": VEML6040IntegrationTime.VEML6040_INTEGRATION_TIME_160MS,
+    "320ms": VEML6040IntegrationTime.VEML6040_INTEGRATION_TIME_320MS,
+    "640ms": VEML6040IntegrationTime.VEML6040_INTEGRATION_TIME_640MS,
+    "1280ms": VEML6040IntegrationTime.VEML6040_INTEGRATION_TIME_1280MS,
+}
+
+
+CONFIG_SCHEMA = (
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(VEML6040Component),
+            cv.Optional(CONF_ILLUMINANCE): sensor.sensor_schema(
+                unit_of_measurement=UNIT_LUX,
+                icon=ICON_LIGHTBULB,
+                accuracy_decimals=1,
+                device_class=DEVICE_CLASS_ILLUMINANCE,
+                state_class=STATE_CLASS_MEASUREMENT,
+            ),
+            cv.Optional(CONF_COLOR_TEMPERATURE): sensor.sensor_schema(
+                unit_of_measurement=UNIT_KELVIN,
+                icon=ICON_THERMOMETER,
+                accuracy_decimals=1,
+                state_class=STATE_CLASS_MEASUREMENT,
+            ),
+            # Color sensors
+            cv.Optional(CONF_RED): sensor.sensor_schema(
+                unit_of_measurement="",
+                icon="mdi:circle",
+                accuracy_decimals=1,
+                state_class=STATE_CLASS_MEASUREMENT,
+            ),
+            cv.Optional(CONF_GREEN): sensor.sensor_schema(
+                unit_of_measurement="",
+                icon="mdi:circle",
+                accuracy_decimals=1,
+                state_class=STATE_CLASS_MEASUREMENT,
+            ),
+            cv.Optional(CONF_BLUE): sensor.sensor_schema(
+                unit_of_measurement="",
+                icon="mdi:circle",
+                accuracy_decimals=1,
+                state_class=STATE_CLASS_MEASUREMENT,
+            ),
+            cv.Optional(CONF_WHITE): sensor.sensor_schema(
+                unit_of_measurement="",
+                icon="mdi:circle",
+                accuracy_decimals=1,
+                state_class=STATE_CLASS_MEASUREMENT,
+            ),
+            cv.Optional(CONF_INTEGRATION_TIME, default="80ms"): cv.enum(
+                VEML6040_INTEGRATION_TIMES, lower=True
+            ),
+            cv.Optional(CONF_GLASS_ATTENUATION_FACTOR, default=0.5): cv.float_range(),
+        }
+    )
+    .extend(cv.polling_component_schema("60s"))
+    .extend(i2c.i2c_device_schema(0x10))
+)
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await i2c.register_i2c_device(var, config)
+
+    cg.add(var.set_integration_time(config[CONF_INTEGRATION_TIME]))
+    cg.add(var.set_glass_attenuation_factor(config[CONF_GLASS_ATTENUATION_FACTOR]))
+
+    if CONF_ILLUMINANCE in config:
+        sens = await sensor.new_sensor(config[CONF_ILLUMINANCE])
+        cg.add(var.set_illuminance_sensor(sens))
+    if CONF_COLOR_TEMPERATURE in config:
+        sens = await sensor.new_sensor(config[CONF_COLOR_TEMPERATURE])
+        cg.add(var.set_color_temperature_sensor(sens))
+    if CONF_RED in config:
+        sens = await sensor.new_sensor(config[CONF_RED])
+        cg.add(var.set_color_red_sensor(sens))
+    if CONF_GREEN in config:
+        sens = await sensor.new_sensor(config[CONF_GREEN])
+        cg.add(var.set_color_green_sensor(sens))
+    if CONF_BLUE in config:
+        sens = await sensor.new_sensor(config[CONF_BLUE])
+        cg.add(var.set_color_blue_sensor(sens))
+    if CONF_WHITE in config:
+        sens = await sensor.new_sensor(config[CONF_WHITE])
+        cg.add(var.set_color_white_sensor(sens))
---
+++ components/veml6040/veml6040.h
@@ -0,0 +1,89 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/components/sensor/sensor.h"
+#include "esphome/components/i2c/i2c.h"
+
+namespace esphome
+{
+  namespace veml6040
+  {
+
+    enum VEML6040IntegrationTime
+    {
+      VEML6040_INTEGRATION_TIME_40MS = 0x00,
+      VEML6040_INTEGRATION_TIME_80MS = 0x01,
+      VEML6040_INTEGRATION_TIME_160MS = 0x02,
+      VEML6040_INTEGRATION_TIME_320MS = 0x03,
+      VEML6040_INTEGRATION_TIME_640MS = 0x04,
+      VEML6040_INTEGRATION_TIME_1280MS = 0x05,
+    };
+
+    class VEML6040Component : public PollingComponent, public i2c::I2CDevice
+    {
+    public:
+      void set_integration_time(VEML6040IntegrationTime integration_time);
+      void set_glass_attenuation_factor(float ga);
+
+      void set_illuminance_sensor(sensor::Sensor *illuminance_sensor) { illuminance_sensor_ = illuminance_sensor; }
+      void set_color_temperature_sensor(sensor::Sensor *color_temperature_sensor)
+      {
+        color_temperature_sensor_ = color_temperature_sensor;
+      }
+      void set_color_red_sensor(sensor::Sensor *color_red_sensor)
+      {
+        color_red_sensor_ = color_red_sensor;
+      }
+      void set_color_green_sensor(sensor::Sensor *color_green_sensor)
+      {
+        color_green_sensor_ = color_green_sensor;
+      }
+      void set_color_blue_sensor(sensor::Sensor *color_blue_sensor)
+      {
+        color_blue_sensor_ = color_blue_sensor;
+      }
+      void set_color_white_sensor(sensor::Sensor *color_white_sensor)
+      {
+        color_white_sensor_ = color_white_sensor;
+      }
+
+      void setup() override;
+      float get_setup_priority() const override;
+      void update() override;
+      void dump_config() override;
+
+    protected:
+      i2c::ErrorCode read_data_register_(uint8_t a_register, uint16_t &data)
+      {
+        uint8_t buffer[2];
+        auto retval = this->read_register(a_register, buffer, 2);
+        if (retval == i2c::ERROR_OK) {
+          data = (uint16_t(buffer[1]) << 8) | (uint16_t(buffer[0]) & 0xFF);
+        }
+        return retval;
+      }
+      i2c::ErrorCode write_config_register_(uint8_t a_register, uint8_t *data, size_t len)
+      {
+        return this->write_register(a_register, data, len);
+      }
+      sensor::Sensor *illuminance_sensor_{nullptr};
+      sensor::Sensor *color_temperature_sensor_{nullptr};
+      sensor::Sensor *color_red_sensor_{nullptr};
+      sensor::Sensor *color_green_sensor_{nullptr};
+      sensor::Sensor *color_blue_sensor_{nullptr};
+      sensor::Sensor *color_white_sensor_{nullptr};
+      float sensitivity{0.12584};
+      float glass_attenuation_{0.5};
+      float illuminance_;
+      float color_temperature_;
+      float red_;
+      float green_;
+      float blue_;
+      float white_;
+
+    private:
+      void calculate_temperature_and_lux_(uint16_t r, uint16_t g, uint16_t b, uint16_t c);
+      uint16_t integration_reg_{VEML6040_INTEGRATION_TIME_80MS};
+    };
+  }
+}
